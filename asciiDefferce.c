c45656b19260e31d8412044aba53f420eb20f434
Rahisul Haque <rhaque@e1z3r5p23.42.us.org>
2017-09-20 20:19:40 -0700

    first commit

---
+++ asciiDefferce.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   asciiDefferce.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhaque <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/20 20:01:48 by rhaque            #+#    #+#             */
+/*   Updated: 2017/09/20 20:17:28 by rhaque           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		main(void)
+{
+	printf("The  differnce between ASCII value of E and A is %d\n", 'E'- 'A');
+	printf("The  differnce between ASCII value of e and a is %d\n", 'e' - 'a');
+
+	printf("The  differnce between ASCII value of 3 and  0 is %d\n", '3'- '0');
+	printf("The  differnce between ASCII value of 6 and 0is %d\n", '6'-'0');
+
+	
+	
+	return (0);
+}
+
